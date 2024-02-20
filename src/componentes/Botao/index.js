5bff9cc122d0eb8c2fffede244da89dd312a1288
Aleir Sanches <aleir.sanches@hotmail.com>
2024-02-20 00:01:04 -0300

    configurando projeto

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -3,9 +3,9 @@ import {TouchableOpacity, Text} from 'react-native';
 import funcaoEstilos from './estilos';
 
 
-export default function Botao({pequeno =false, invertido = false,valor, acao}){
+export default function Botao({pequeno =false, invertido = false,valor, acao, estilos}){
     const estlosPadrao = funcaoEstilos(pequeno, invertido)
-    return <TouchableOpacity onPress={acao} style={estlosPadrao.botao}>
+    return <TouchableOpacity onPress={acao} style={[estlosPadrao.botao, estilos ]}>
         <Text style={estlosPadrao.valor}>{valor}</Text>
     </TouchableOpacity>
 }
